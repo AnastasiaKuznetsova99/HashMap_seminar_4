66747a5226e037a201c9d2831377e851850af579
Anastasia Kuznetsova <asiakisa11@gmail.com>
2023-10-01 18:43:52 +0500

    lesson 4

---
+++ Employe.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.lesson4;
+
+public class Employe {
+
+    private String name;
+
+    private int age;
+
+    public Employe(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
